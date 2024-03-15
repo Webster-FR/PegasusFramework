aa7b9537fc0f1de28269b28916d6bcd9da5c2d8e
loicmaes <production@maesloic.fr>
2024-03-15 13:10:59 +0100

    :rocket: Create a base to start a minecraft plugin on the server

---
+++ src/main/java/fr/pegasus/papermc/samples/PluginStarter.java
@@ -0,0 +1,21 @@
+package fr.pegasus.papermc.samples;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class PluginStarter extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
