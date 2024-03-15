b93f29da5af968dbbea88c259ca552c66bcb632b
loicmaes <production@maesloic.fr>
2024-03-15 15:17:32 +0100

    :rocket: Setup book builder

---
+++ src/main/java/fr/pegasus/papermc/builders/items/BookBuilder.java
@@ -0,0 +1,57 @@
+package fr.pegasus.papermc.builders.items;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class BookBuilder extends ItemBuilder {
+    // FIELDS
+    private final BookMeta meta;
+
+    // CONSTRUCTORS
+    public BookBuilder () {
+        super(Material.WRITTEN_BOOK);
+        this.meta = (BookMeta) super.getMeta();
+    }
+    public BookBuilder (final @NotNull ItemStack item) {
+        super(item);
+        this.meta = (BookMeta) super.getMeta();
+    }
+    public BookBuilder (final int amount) {
+        super(Material.WRITTEN_BOOK, amount);
+        this.meta = (BookMeta) super.getMeta();
+    }
+
+    // SETTERS
+    public @NotNull BookBuilder setAuthor (final @Nullable String author) {
+        this.meta.setAuthor(author);
+        return this;
+    }
+    public @NotNull BookBuilder addPages (final @NotNull Component... pages) {
+        this.meta.addPages(pages);
+        return this;
+    }
+    public @NotNull BookBuilder setGeneration (final @Nullable BookMeta.Generation generation) {
+        this.meta.setGeneration(generation);
+        return this;
+    }
+    public @NotNull BookBuilder setTitle (final @Nullable String title) {
+        this.meta.setTitle(title);
+        return this;
+    }
+
+    // BUILD
+    @Override
+    public @NotNull ItemStack make () {
+        return super.make(this.meta);
+    }
+
+    // CLONE
+    @Override
+    public @NotNull BookBuilder clone () {
+        return (BookBuilder) super.clone();
+    }
+}
