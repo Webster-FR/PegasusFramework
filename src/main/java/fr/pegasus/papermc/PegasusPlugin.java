0e2ac2ca734633699806421a9060b7c6aa0b61cc
Xen0Xys <red57101@gmail.com>
2024-03-16 03:59:03 +0100

    :white_check_mark: Change main plugin class to allow better testing

--- src/main/java/fr/pegasus/papermc/PegasusPlugin.java
+++ src/main/java/fr/pegasus/papermc/PegasusPlugin.java
@@ -3,14 +3,18 @@ package fr.pegasus.papermc;
 import fr.pegasus.papermc.managers.ServerManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 @SuppressWarnings("unused")
 public class PegasusPlugin extends JavaPlugin {
 
+    public static Logger logger = Logger.getLogger("PegasusFramework");
     private ServerManager serverManager;
 
     @Override
     public void onLoad() {
         super.onLoad();
+        logger.info("Pegasus Framework has been loaded");
     }
 
     @Override
@@ -18,11 +22,13 @@ public class PegasusPlugin extends JavaPlugin {
         super.onEnable();
         serverManager = new ServerManager(this);
         serverManager.initLobby();
+        logger.info("Pegasus Framework has been enabled");
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+        logger.info("Pegasus Framework has been disabled");
     }
 
     public ServerManager getServerManager() {
