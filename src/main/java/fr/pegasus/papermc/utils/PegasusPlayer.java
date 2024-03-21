e5e7b1bdb6cefe25623ba003f9ec586853e8ef5e
Xen0Xys <red57101@gmail.com>
2024-03-21 11:45:28 +0100

    :memo: Add documentation for PegasusPlayer

--- src/main/java/fr/pegasus/papermc/utils/PegasusPlayer.java
+++ src/main/java/fr/pegasus/papermc/utils/PegasusPlayer.java
@@ -9,14 +9,26 @@ import java.util.Objects;
 public class PegasusPlayer {
     private final String name;
 
+    /**
+     * Create a new PegasusPlayer
+     * @param name The player name
+     */
     public PegasusPlayer(final @NotNull String name) {
         this.name = name;
     }
 
+    /**
+     * Check if the player is online
+     * @return True if the player is online, false otherwise
+     */
     public boolean isOnline(){
         return Objects.nonNull(Bukkit.getPlayer(this.name));
     }
 
+    /**
+     * Get the player if online, throw a {@link RuntimeException} otherwise
+     * @return The player if online
+     */
     public Player getPlayer(){
         Player player = Bukkit.getPlayer(this.name);
         if(Objects.isNull(player))
@@ -24,6 +36,11 @@ public class PegasusPlayer {
         return player;
     }
 
+    /**
+     * Check if an object is equals to this PegasusPlayer
+     * @param obj The object to compare
+     * @return True if the object is equals to this PegasusPlayer, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if(!(obj instanceof PegasusPlayer))
@@ -31,6 +48,10 @@ public class PegasusPlayer {
         return ((PegasusPlayer) obj).name.equals(this.name);
     }
 
+    /**
+     * Get the hash code of this PegasusPlayer
+     * @return The hash code of this PegasusPlayer
+     */
     @Override
     public int hashCode() {
         return this.name.hashCode();
