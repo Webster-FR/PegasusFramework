dc059a17644e2f2720d29d25814a650c56fd8bfd
Xen0Xys <red57101@gmail.com>
2024-03-22 09:58:02 +0100

    :white_check_mark: Add minor testing behavior for the sample instance

--- src/main/java/fr/pegasus/papermc/samples/game/SampleInstance.java
+++ src/main/java/fr/pegasus/papermc/samples/game/SampleInstance.java
@@ -5,7 +5,10 @@ import fr.pegasus.papermc.games.instances.enums.InstanceStates;
 import fr.pegasus.papermc.games.options.CommonOptions;
 import fr.pegasus.papermc.games.options.InstanceOptions;
 import fr.pegasus.papermc.scores.ScoreManager;
+import fr.pegasus.papermc.utils.PegasusPlayer;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SampleInstance extends Instance {
@@ -35,7 +38,12 @@ public class SampleInstance extends Instance {
 
     @Override
     public void onRoundStart() {
-
+        for(PegasusPlayer pPlayer : this.getPlayerManager().getPlayers()){
+            if(!pPlayer.isOnline())
+                continue;
+            Player player = pPlayer.getPlayer();
+            player.getInventory().addItem(new ItemStack(Material.BEDROCK, 1));
+        }
     }
 
     @Override
@@ -49,7 +57,13 @@ public class SampleInstance extends Instance {
     }
 
     @Override
-    public void onPlayerReconnect(Player player, InstanceStates disconnectState, InstanceStates reconnectState) {
+    public void onTick(int remainingTime) {
 
+    }
+
+    @Override
+    public void onPlayerReconnect(Player player, InstanceStates disconnectState, InstanceStates reconnectState) {
+        if(disconnectState != InstanceStates.ROUND_STARTED && reconnectState == InstanceStates.ROUND_STARTED)
+            player.getInventory().addItem(new ItemStack(Material.BEDROCK, 1));
     }
 }
