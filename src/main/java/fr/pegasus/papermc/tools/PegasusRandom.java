e075d4c303a2e87e65a1bb947772b02f3f1cc453
Xen0Xys <red57101@gmail.com>
2024-03-22 11:35:56 +0100

    :rocket: Add constructor with random seed and new method for random strings

--- src/main/java/fr/pegasus/papermc/tools/PegasusRandom.java
+++ src/main/java/fr/pegasus/papermc/tools/PegasusRandom.java
@@ -1,11 +1,33 @@
 package fr.pegasus.papermc.tools;
 
+import java.util.Random;
+
 public class PegasusRandom {
+    private final Random random;
+
+    public PegasusRandom(){
+        this.random = new Random();
+    }
+
+    public PegasusRandom(long seed){
+        this.random = new Random(seed);
+    }
+
     public int nextInt(int max){
         return this.nextInt(0, max);
     }
 
     public int nextInt(int min, int max){
-        return new PegasusRandom().nextInt(max - min) + min;
+        return random.nextInt(max - min) + min;
+    }
+
+    public String randomString(int length){
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder result = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            result.append(characters.charAt(index));
+        }
+        return result.toString();
     }
 }
